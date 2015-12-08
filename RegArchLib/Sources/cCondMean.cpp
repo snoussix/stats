87f3d4d257f351d293837b18903d210641cb920c
Marion-ly Vu-ngoc <vungocma@ensimag.grenoble-inp.fr>
2015-12-08 08:09:13 +0100

    a

--- RegArchLib/Sources/cCondMean.cpp
+++ RegArchLib/Sources/cCondMean.cpp
@@ -1,14 +1,14 @@
 #include "cCondMean.hpp"
 #include <vector>
 
-double CCONDMEAN::mComputeMean(CRegArchData mdata,int t){
+double CCondMean::mComputeMean(CRegArchData mdata,int t){
 //TODO 
 return 0.0;
 }
 
 
 
-std::vector<double> CCONDMEAN::mComputeGrad(CRegArchData mdata,int t){
+std::vector<double> CCondMean::mComputeGrad(CRegArchData mdata,int t){
 //TODO
 std::vector<double> reda; 
 return reda;
